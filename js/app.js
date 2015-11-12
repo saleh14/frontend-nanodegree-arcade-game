4f86b1e6d7208ce0c6748b23bddc4cb2c479773c
unknown <mirajsm@CC026951.aramco.com>
2015-11-12 15:41:17 +0300

    refactoring in app.js

--- js/app.js
+++ js/app.js
@@ -8,6 +8,14 @@ Player.prototype.init = function () {
 };
 
 Player.prototype.update = function () {
+    var currentEnemies = allEnemies.slice(-4);
+    var self = this;
+    currentEnemies.forEach(function(enemy) {
+        if (Math.abs(self.x - enemy.x) < 70) 
+                && Math.abs(self.y - enemy.y) < 70 )
+            init();
+        }
+    }
 
 };
 
@@ -50,42 +58,24 @@ Enemy.prototype.update = function (dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-
-    if (nextEnemyIsReady){
-        nextEnemyIsReady = false;
-        var ran = Math.floor(Math.random() * 3);
-        allEnemies.push(new Enemy(-300,enemiesY[ran],2));
-    }
-
-    this.x += this.speed;
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function () {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
-function initEnemies() {
-    allEnemies = [new Enemy(-300,62,2)],
+}
+function init(global) {
+    global.allEnemies = [new Enemy(-300,62,2)],
+    global.nextEnemyIsReady = false,
+    global.enemiesY = [62,145,225],
+    global.player = new Player();
+    player.init();
     nextEnemy();
 }
+
 function nextEnemy() {
     var ran = Math.random() + 1,
-        period = ran * 1000;
+        period = ran * 1100;
     nextEnemyIsReady = true;
     return setTimeout(nextEnemy, period);
-
 };
 
-var nextEnemyIsReady = false,
-    enemiesY = [62,145,225];
-player = new Player();
-player.init();
-initEnemies();
+init(this);
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
